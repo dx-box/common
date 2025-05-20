c1ab31a4bf8b65e986eff67bed3b0f7029e87eda
youtaek_jung <youtaek_jung@tmaxsoft.co.kr>
2025-05-20 14:05:27 +0900

    refactor: apply configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,32 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "prettier"
   ],
-  "rules": {}
+  "rules": {
+    "no-console": "warn",
+    "no-debugger": "error",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        "argsIgnorePattern": "^_"
+      }
+    ],
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/ban-ts-comment": "warn",
+    "eqeqeq": [
+      "error",
+      "always"
+    ],
+    "curly": [
+      "error",
+      "all"
+    ],
+    "no-duplicate-imports": "error",
+    "prefer-const": "error",
+    "object-shorthand": [
+      "error",
+      "always"
+    ]
+  }
 };
