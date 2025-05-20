4aed328b8216e6490efe0b714de5ddd3e702448e
youtaekjung <jyt9319@gmail.com>
2025-05-20 21:27:37 +0900

    refactor: format .eslintrc with prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,45 +1,30 @@
 module.exports = {
-  "root": true,
-  "env": {
-    "browser": true,
-    "node": true,
-    "es2021": true
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
   },
-  "parser": "@typescript-eslint/parser",
-  "plugins": [
-    "@typescript-eslint"
-  ],
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier"
-  ],
-  "rules": {
-    "no-console": "warn",
-    "no-debugger": "error",
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  rules: {
+    'no-console': 'warn',
+    'no-debugger': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
       {
-        "argsIgnorePattern": "^_"
-      }
+        argsIgnorePattern: '^_',
+      },
     ],
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/ban-ts-comment": "warn",
-    "eqeqeq": [
-      "error",
-      "always"
-    ],
-    "curly": [
-      "error",
-      "all"
-    ],
-    "no-duplicate-imports": "error",
-    "prefer-const": "error",
-    "object-shorthand": [
-      "error",
-      "always"
-    ]
-  }
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/ban-ts-comment': 'warn',
+    eqeqeq: ['error', 'always'],
+    curly: ['error', 'all'],
+    'no-duplicate-imports': 'error',
+    'prefer-const': 'error',
+    'object-shorthand': ['error', 'always'],
+  },
 };
