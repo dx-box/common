f21a727b86e81cdd94438eece28b72f39447a9cd
youtaekjung <jyt9319@gmail.com>
2025-05-28 20:19:54 +0900

    build: update build output

--- dist/bin/dx-fix.js
+++ dist/bin/dx-fix.js
@@ -1,2 +1,2 @@
 #!/usr/bin/env node
-import{execSync as n}from"child_process";const x=()=>{n("npx dx-absolut"),n("npx dx-lint"),n("npx dx-format")};x();
+import{execSync as n}from"child_process";const o=()=>{n("npx dx-absolute"),n("npx dx-lint"),n("npx dx-format"),console.log("\u2705 Successfully updated tsconfig.json paths and applied linting & formatting.")};o();
